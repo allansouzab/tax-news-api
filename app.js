a7a5b28e5fe18087f29d37975872dedd4053faef
Allan Souza <allansouzab@hotmail.com>
2020-07-08 16:39:06 -0300

    Incluindo tratamento de body

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const newsRoute = require('./routes/news');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/api/news', newsRoute);
 
 app.use((error, req, res, next) => {
